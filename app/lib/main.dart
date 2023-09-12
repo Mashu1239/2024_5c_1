2069f0edb4152ae8c5cf170d227d9f98bccd47c3
Mashu <mateusz.ortyl.kontakt1@gmail.com>
2023-09-12 13:46:48 +0200

    Button5

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -12,29 +12,64 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int ButtonCount = 1;
+  String Tekst = "Przycisk nie zostal wcisniety";
+  bool InProgress = false;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Apka'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.add_alert),
+              tooltip: 'Show Snackbar',
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('This is a snackbar')));
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.accessible_forward_rounded),
+              tooltip: 'Show Snackbar',
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('This is a snackbar')));
+              },
+            ),
+          ],
         ),
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text('Tekst'),
-              ElevatedButton(
-                onPressed: () {
-                  ButtonCount += 1;
-                },
-                child: Text('Klik'),
+        body: Builder(
+          builder: (BuildContext context) {
+            return Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(Tekst),
+                  ElevatedButton(
+                    onPressed: _ChangeText,
+                    child: InProgress
+                        ? CircularProgressIndicator()
+                        : Text("Zmien tekst"),
+                  ),
+                ],
               ),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
+  }
+
+  void _ChangeText() {
+    setState(() {
+      InProgress = true;
+      Future.delayed(Duration(milliseconds: 3000), () {
+        setState(() {
+          InProgress = false;
+          Tekst = "Przycisko zostal nacisniety";
+        });
+      });
+    });
   }
 }
